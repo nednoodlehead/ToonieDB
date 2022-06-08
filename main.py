ef64dae04d295457a6da8a16daabfdc1717e3ed3
NedNoodleHead <spencer.braak@gmail.com>
2022-06-08 15:01:18 -0700

    fix for age_entry having a default value

--- main.py
+++ main.py
@@ -43,6 +43,8 @@ name_entry = ttk.Entry(add_data_frame, textvariable=name_var)
 
 age_label = ttk.Label(add_data_frame, text="Age")
 age_entry = ttk.Entry(add_data_frame, textvariable=age_var)
+# Default value is '0'. .delete() gets rid of that. It has no proper purpose
+age_entry.delete("0")
 
 gender_label = ttk.Label(add_data_frame, text="Gender")
 gender_entry = ttk.Entry(add_data_frame, textvariable=gender_var)
@@ -52,17 +54,17 @@ contact_type_entry = ttk.Entry(add_data_frame, textvariable=contact_type_var)
 
 problem_label = ttk.Label(add_data_frame, text="Problem:")
 # tk.Text needed because ttk.Entry cannot support multiple lines
-problem_entry = tk.Text(add_data_frame, width=50, height=5, font=("Arial", 10))
+problem_entry = tk.Text(add_data_frame, width=40, height=5, font=("Arial", 10))
 
 # tk.Text cannot map to a StringVar() so the output is down here :)
 problem_var = problem_entry.get('1.0', 'end')
 
 solution_label = ttk.Label(add_data_frame, text="Solution:")
-solution_entry = tk.Text(add_data_frame, width=50, height=5, font=("Arial", 10))
+solution_entry = tk.Text(add_data_frame, width=40, height=5, font=("Arial", 10))
 
 solution_var = problem_entry.get('1.0', 'end')
 
-device_type_label = ttk.Label(add_data_frame, text="Device Type:")
+device_type_label = ttk.Label(add_data_frame, text="Device Type: (PC, Phone)")
 device_type_entry = ttk.Entry(add_data_frame, textvariable=device_type_var)
 
 date_of_service_label = ttk.Label(add_data_frame, text="Date of service: (YY/MM/DD)")
@@ -71,16 +73,52 @@ date_of_service_entry = ttk.Entry(add_data_frame, textvariable=date_of_service_v
 date_of_contact_label = ttk.Label(add_data_frame, text="Date of Contact: (YY/MM/DD)")
 date_of_contact_entry = ttk.Entry(add_data_frame, textvariable=date_of_contact_var)
 
-time_of_contact_label = ttk.Label(add_data_frame, text="Time of contact:")
+time_of_contact_label = ttk.Label(add_data_frame, text="Time of contact: (8PM)")
 time_of_contact_entry = ttk.Entry(add_data_frame, textvariable=time_of_contact_var)
 
-difficulty_label = ttk.Label(add_data_frame, text="Difficulty")
+difficulty_label = ttk.Label(add_data_frame, text="Difficulty (1-10)")
 difficulty_entry = ttk.Entry(add_data_frame, textvariable=difficulty_var)
 
-payment_label = ttk.Label(add_data_frame, text="Payment")
+payment_label = ttk.Label(add_data_frame, text="Payment (8) as in 8 dollars")
 payment_entry = ttk.Entry(add_data_frame, textvariable=payment_var)
 
 # Placing all the Widgets & Labels
+
+name_label.place(x=20, y=20)
+name_entry.place(x=20, y=40)
+
+age_label.place(x=20, y=80)
+age_entry.place(x=20, y=100)
+
+gender_label.place(x=20, y=140)
+gender_entry.place(x=20, y=160)
+
+contact_type_label.place(x=20, y=200)
+contact_type_entry.place(x=20, y=220)
+
+problem_label.place(x=450, y=20)
+problem_entry.place(x=450, y=40)
+
+solution_label.place(x=450, y=130)
+solution_entry.place(x=450, y=150)
+
+device_type_label.place(x=20, y=260)
+device_type_entry.place(x=20, y=280)
+
+date_of_service_label.place(x=20, y=320)
+date_of_service_entry.place(x=20, y=340)
+
+date_of_contact_label.place(x=20, y=380)
+date_of_contact_entry.place(x=20, y=400)
+
+time_of_contact_label.place(x=20, y=440)
+time_of_contact_entry.place(x=20, y=460)
+
+difficulty_label.place(x=20, y=500)
+difficulty_entry.place(x=20, y=520)
+
+payment_label.place(x=20, y=560)
+payment_entry.place(x=20, y=580)
 
 
 
