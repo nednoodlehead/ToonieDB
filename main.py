be16ac7b4fd9c7c462f4f973f55dd1d0ae42de51
NedNoodleHead <spencer.braak@gmail.com>
2022-06-08 15:41:53 -0700

    -More fields added for checkbuttons

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ root.resizable(False, False)
 # Defining the style for the two tkinter frames
 main_style = ttk.Style()
 main_style.configure("TFrame", background="#656489")
-
+checkbox_style = ttk.Style()
+checkbox_style.configure("default.TCheckbutton", background="#656489")
 
 master_notebook = ttk.Notebook(root)
 view_data_frame = ttk.Frame(master_notebook, width=825, height=700, style='TFrame')
@@ -23,7 +24,7 @@ master_notebook.add(view_data_frame, text="View Data")
 view_data_frame.configure()
 
 # Variables that ttk.Entry & tk.Text outputs to. Defined as data_var.
-
+# Must have some type of function to sort through the checkboxes and detirmines which has value. and use it
 name_var = tk.StringVar()
 age_var = tk.IntVar()
 gender_var = tk.StringVar()
@@ -46,11 +47,28 @@ age_entry = ttk.Entry(add_data_frame, textvariable=age_var)
 # Default value is '0'. .delete() gets rid of that. It has no proper purpose
 age_entry.delete("0")
 
-gender_label = ttk.Label(add_data_frame, text="Gender")
-gender_entry = ttk.Entry(add_data_frame, textvariable=gender_var)
+gender_label = ttk.Label(add_data_frame, text="Male    Female   Other")
 
+# Unsure if not adding an offvalue= implies 'NoneType'. Which is what i want. Leaving for now
+gender_entry_male = ttk.Checkbutton(add_data_frame, onvalue="Male", style="default.TCheckbutton")
+gender_entry_female = ttk.Checkbutton(add_data_frame, onvalue="Female", style="default.TCheckbutton")
+gender_entry_other = ttk.Checkbutton(add_data_frame, onvalue="Other", style="default.TCheckbutton")
+
+# Clears the boxes of the weird default state. To an unchecked state
+gender_entry_male.state(["!alternate"])
+gender_entry_female.state(["!alternate"])
+gender_entry_other.state(["!alternate"])
+
+# gender_entry = ttk.Entry(add_data_frame, textvariable=gender_var)
 contact_type_label = ttk.Label(add_data_frame, text="Contact Type:")
-contact_type_entry = ttk.Entry(add_data_frame, textvariable=contact_type_var)
+contact_type_info = ttk.Label(add_data_frame, text="FaceBook    Email   Phone")
+contact_type_entry_facebook = ttk.Checkbutton(add_data_frame, onvalue="FaceBook", style="default.TCheckbutton")
+contact_type_entry_email = ttk.Checkbutton(add_data_frame, onvalue="Email", style="default.TCheckbutton")
+contact_type_entry_phone = ttk.Checkbutton(add_data_frame, onvalue="Phone", style="default.TCheckbutton")
+
+contact_type_entry_facebook.state(["!alternate"])
+contact_type_entry_email.state(["!alternate"])
+contact_type_entry_phone.state(["!alternate"])
 
 problem_label = ttk.Label(add_data_frame, text="Problem:")
 # tk.Text needed because ttk.Entry cannot support multiple lines
@@ -64,8 +82,17 @@ solution_entry = tk.Text(add_data_frame, width=40, height=5, font=("Arial", 10))
 
 solution_var = problem_entry.get('1.0', 'end')
 
-device_type_label = ttk.Label(add_data_frame, text="Device Type: (PC, Phone)")
-device_type_entry = ttk.Entry(add_data_frame, textvariable=device_type_var)
+device_type_label = ttk.Label(add_data_frame, text="Desktop    Phone    Laptop")
+device_type_label_other = ttk.Label(add_data_frame, text="Other?")
+device_type_entry_desktop = ttk.Checkbutton(add_data_frame, onvalue="Desktop", style="default.TCheckbutton")
+device_type_entry_phone = ttk.Checkbutton(add_data_frame, onvalue="Phone", style="default.TCheckbutton")
+device_type_entry_laptop = ttk.Checkbutton(add_data_frame, onvalue="Laptop", style="default.TCheckbutton")
+device_type_entry_other = ttk.Entry(add_data_frame, textvariable=device_type_var)
+
+device_type_entry_desktop.state(["!alternate"])
+device_type_entry_phone.state(["!alternate"])
+device_type_entry_laptop.state(["!alternate"])
+
 
 date_of_service_label = ttk.Label(add_data_frame, text="Date of service: (YY/MM/DD)")
 date_of_service_entry = ttk.Entry(add_data_frame, textvariable=date_of_service_var)
@@ -91,10 +118,15 @@ age_label.place(x=20, y=80)
 age_entry.place(x=20, y=100)
 
 gender_label.place(x=20, y=140)
-gender_entry.place(x=20, y=160)
+gender_entry_male.place(x=20, y=160)
+gender_entry_female.place(x=65, y=160)
+gender_entry_other.place(x=110, y=160)
 
-contact_type_label.place(x=20, y=200)
-contact_type_entry.place(x=20, y=220)
+contact_type_label.place(x=20, y=180)
+contact_type_info.place(x=20, y=200)
+contact_type_entry_facebook.place(x=30, y=220)
+contact_type_entry_email.place(x=85, y=220)
+contact_type_entry_phone.place(x=140, y=220)
 
 problem_label.place(x=450, y=20)
 problem_entry.place(x=450, y=40)
@@ -102,8 +134,12 @@ problem_entry.place(x=450, y=40)
 solution_label.place(x=450, y=130)
 solution_entry.place(x=450, y=150)
 
-device_type_label.place(x=20, y=260)
-device_type_entry.place(x=20, y=280)
+device_type_label.place(x=20, y=240)
+device_type_label_other.place(x=160, y=280)
+device_type_entry_desktop.place(x=30, y=260)
+device_type_entry_phone.place(x=85, y=260)
+device_type_entry_laptop.place(x=140, y=260)
+device_type_entry_other.place(x=20, y=280)
 
 date_of_service_label.place(x=20, y=320)
 date_of_service_entry.place(x=20, y=340)
