cdeb6e6f6a2ca786ecbfb918c9e527db56deb62a
NedNoodleHead <spencer.braak@gmail.com>
2022-06-11 18:57:14 -0700

    Added missing field

--- insert_data.py
+++ insert_data.py
@@ -1,16 +1,20 @@
 import sqlite3
+import tkinter.messagebox
+from tkinter import messagebox
 from main import uuid_gen
 # Use uuid_gen() to create the unique ID
 
 
-def add_data(name, age, gender, contact, contact_info, problem, solution, device_type, date_of_contact, date_of_service,
+def add_data(name, age, gender, contact_type, contact_info, problem, solution, device_type, time_of_contact,
+             date_of_contact, date_of_service,
              difficulty, payment, id): #13
     con = sqlite3.connect("./MAIN.DB")
     cur = con.cursor()
-    cur.execute("INSERT INTO {} VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)".format(name, age, gender, contact, contact_info,
-                                                                           problem, solution, device_type,
-                                                                           date_of_contact, date_of_service,difficulty,
-                                                                           payment, id))
+    cur.execute("INSERT INTO {} VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)".format(name, age, gender, contact_type,
+                                                                           contact_info, problem, solution, device_type,
+                                                                           time_of_contact, date_of_contact,
+                                                                           date_of_service, difficulty, payment, id))
     con.commit()
+    tkinter.messagebox.Message("Data Entered Successfully")
 
 
